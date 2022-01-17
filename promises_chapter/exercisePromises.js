67ede1ce7f7f87c036328cdd235a3f65073b13e3
lenindelarosa <leninmanueld@gmail.com>
2022-01-17 18:09:08 -0400

    exercise with promises

--- promises_chapter/exercisePromises.js
+++ promises_chapter/exercisePromises.js
@@ -44,3 +44,14 @@ Promise.all(urls.map(url=>
 
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
+
+
+Promise.all(urls.map(url=>
+  fetch(url).then(people=> people.json())
+)).then(array => {
+  console.log('1', array[0])
+  console.log('2', array[1])
+  console.log('3', array[2])
+  console.log('4', array[3])
+})
+.catch(err => console.log('ughhhh fix it!', err));
